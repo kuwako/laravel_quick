6421ef77337033a5c1023301fc4b2f2f75d7ac0a
kuwako <m.kuwako0702@gmail.com>
2018-10-17 07:54:31 +0000

    routing書いたけどポートフォワーディングがうまくいってない

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/task', function(Request $request) {
+    //
+});
+
+Route::delete('/task/{task}', function(Task $task) {
+    //
 });
